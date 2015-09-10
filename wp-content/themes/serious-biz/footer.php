f80b6dc5d3fb128495bb7d4785e3f35be3480ca9
aroldoa <aroldo@primomotif.com>
2015-09-10 17:59:49 -0500

    Uploaded Wordpress
    Uploaded Wordpress and custom business theme.

---
+++ wp-content/themes/serious-biz/footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Serious-Biz
+ */
+
+?>
+
+	</div><!-- #content -->
+<div class="row">
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+		
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div>
+</div><!-- #page -->
+<div class="copyright">
+	copyright &copy; <?php echo date('Y'); ?> Small Business Theme, All Rights Reserved. 
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
